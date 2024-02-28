b1f621ab62702d549a278d032ed510d6fd8e1ceb
Kent <kentnielsen26@gmail.com>
2024-02-28 18:55:49 +0100

    Added script and data to compute number of recomputations

---
+++ accessPatterToRecomputation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# Load data from test/saved-results/access-pattern into array
+def load_file_into_array(filename):
+    with open(f"test/saved-results/access-pattern/{filename}", 'r') as file:
+        data = file.readlines()
+        #convert data to int
+        data = [int(x.strip()) for x in data]
+        return data
+
+# find largest value in data
+def find_largest(data):
+    largest = 0
+    for i in range(0, len(data)):
+        if data[i] > largest:
+            largest = data[i]
+    return largest
+
+# Go over data from file and compute number of recomputation
+def compute_recomputation(data, cache_size, largest):
+    start_idx = 0   
+    recomputation = 1
+    for i in range(0, len(data)):
+        if (data[i] > start_idx + cache_size or data[i] < start_idx):
+            recomputation += 1
+            if (data[i] + cache_size < largest):
+                start_idx = data[i]
+            else:
+                start_idx = largest - cache_size
+    return recomputation
+
+
+# Main function
+if __name__ == '__main__':
+    # Load data from file
+    variant = "256"
+    data = load_file_into_array(f"vk_box/{variant}F.txt")
+    largest = find_largest(data)
+    # Go over all cache sizes from 19 to largest and store in an array
+    recomputation = []
+    for i in range(19, largest+1):
+        recomputation.append(compute_recomputation(data, i, largest))
+
+    # Write recomputation to file
+    with open(f"test/saved-results/access-pattern/vk_box/comp-{variant}.txt", 'w') as file:
+        for i in range(0, len(recomputation)):
+            file.write(f"{recomputation[i]}\n")    
+

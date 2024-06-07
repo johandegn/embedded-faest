698f003f9c9ca3f23a51231b039fdf740e5e0786
Johan Degn <degnjohan@gmail.com>
2024-06-07 19:56:26 +0200

    Recomputations from access pattern for every final embedded variant

--- accessPatternToRecomputation.py
+++ accessPatternToRecomputation.py
@@ -19,9 +19,12 @@ def find_largest(data):
 # Go over data from file and compute number of recomputation
 def compute_recomputation(data, cache_size, largest):
     start_idx = 0
-    l = 1600 # 128
-    #l = 3264 #192
-    #l = 4000  # 256  
+    #l = 1600  # 128
+    #l = 3264  # 192
+    #l = 4000  # 256
+    #l = 1280  # em128
+    #l = 2304  # em192
+    #l = 3584  # em256
     recomputation = 1
     for i in range(0, len(data)):
         if (data[i] > start_idx + cache_size or data[i] < start_idx):
@@ -40,16 +43,19 @@ def compute_recomputation(data, cache_size, largest):
 # Main function
 if __name__ == '__main__':
     # Load data from file
-    variant = "128"
-    data = load_file_into_array(f"vk_box_cache/{variant}.txt")
+    variant = "em128f_verifier"
+    min_oles = 15
+    data = load_file_into_array(f"final/embedded/{variant}.txt")
     largest = find_largest(data)
     # Go over all cache sizes from 19 to largest and store in an array
     recomputation = []
-    for i in range(19, largest+1):
+    # min_oles-1 since we go from amount to index
+    for i in range(min_oles-1, largest+1):
         recomputation.append(compute_recomputation(data, i, largest))
 
     # Write recomputation to file
-    with open(f"test/saved-results/access-pattern/vk_box_cache/comp-{variant}.txt", 'w') as file:
+    with open(f"test/saved-results/access-pattern/final/embedded-comp/comp-{variant}.txt", 'w') as file:
+        file.write(f"OLEs,comps\n")
         for i in range(0, len(recomputation)):
-            file.write(f"{recomputation[i]}\n")    
+            file.write(f"{i + min_oles},{recomputation[i]}\n")
 
